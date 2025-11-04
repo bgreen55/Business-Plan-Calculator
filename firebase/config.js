c2afa617e5138ce63f1ebf4ee04f83195c9c7adf
Bradley Green <90005776+bgreen55@users.noreply.github.com>
2025-11-03 22:27:36 -0700

    Update config.js for firebase

--- firebase/config.js
+++ firebase/config.js
@@ -1,8 +1,8 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
